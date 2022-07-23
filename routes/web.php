b2f58ef985e52285d9c1277f1cbc0bcffafe9f92
fliderman01 <fliderman01@gmail.com>
2022-07-23 14:57:16 +0400

    5-laravel-routing - Finished task

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/main', function () {
+    return 'welcome to Main page';
+});
+
+Route::get('/blog', function () {
+    return 'welcome to Blog';
+});
+
+Route::get('/profile', function () {
+    return 'welcome to Profile';
+});
+
+Route::get('/about', function () {
+    return 'welcome to About us';
+});
