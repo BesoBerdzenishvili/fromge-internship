b2b2f0c1bd84c8ce45debc409e527b085d6a4f10
fliderman01 <fliderman01@gmail.com>
2022-07-23 15:33:29 +0400

    completed 6-simple-blades

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Fromge profile</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body>
+        <h2>Fromge Profile</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas alias fugiat molestias consequatur rerum.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda eligendi expedita consectetur delectus magnam corrupti repellendus animi nobis! Vero facilis veritatis quo consequatur hic.</p>
+    </body>
+</html>
